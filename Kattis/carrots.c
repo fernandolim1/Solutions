67494e7a0e30c8c5de606aa14319d6fcf98e4c4f
fernandolim1 <fernando.lim11@gmail.com>
2021-01-11 13:13:19 +0700

    Update carrots.c

--- Kattis/carrots.c
+++ Kattis/carrots.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int main() {
+int main(){
     int n, p;
+    char s[1000];
     scanf("%d %d", &n, &p);
-    int i;
-    char c[1000];
-    for (i = 0; i < n; ++i) {
-        scanf("%s", c);
+    for(int i=0;i<n;++i){
+        scanf("%s", s);
     }
     printf("%d\n", p);
+    return 0;   
 }
